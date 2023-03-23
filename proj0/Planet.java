5939879488172b2b072ec437ff5dc52f6eb1f87a
shizukou-beat <119555723+shizukou-beat@users.noreply.github.com>
2023-03-23 13:39:36 +0800

    Update Planet.java

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -4,12 +4,12 @@
 //
 
 public class Planet {
-    double xxPos;
-    double yyPos;
-    double xxVel;
-    double yyVel;
-    double mass;
-    String imgFileName;
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
 
     public Planet(double var1, double var3, double var5, double var7, double var9, String var11) {
         this.xxPos = var1;
