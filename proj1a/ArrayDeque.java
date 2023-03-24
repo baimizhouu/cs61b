f264f18a0512be6f2b4a54a0572defd37b2de4c1
shizukou-beat <119555723+shizukou-beat@users.noreply.github.com>
2023-03-24 13:10:31 +0800

    project 1a

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,2 +1,130 @@
-package PACKAGE_NAME;public class ArrayDeque {
+public class ArrayDeque<T> {
+    private T[] array;
+    private double usage_limit = 0.25;
+    private int size;
+    private int nextLast;
+    private int nextFirst;
+    public ArrayDeque(T item){
+        this.array = (T[]) new Object[8];
+
+        nextFirst = 3;
+        nextLast = 4;
+        this.addFirst(item);
+    }
+    public ArrayDeque(){
+        T[] array = (T[]) new Object[8];
+        size = 0;
+    }
+    private int index_increment(int index,int increment){
+        if(increment == +1){
+            index ++;
+            if(index == array.length){
+                index = 0;
+            }
+        } else if (increment == -1) {
+            index --;
+            if (index == -1){
+                index = array.length-1;
+            }
+        }
+        return index;
+    }
+    public void addFirst(T item){
+        if(this.capacity_check()){
+            this.size_revise((int) (size/usage_limit-1));
+        }
+        array[nextFirst] = item;
+        this.nextFirst = index_increment(nextFirst,-1);
+        size ++;
+    }
+    public void addLast(T item){
+        if(this.capacity_check()){
+            this.size_revise((int) (size/usage_limit-1));
+        }
+        array[nextLast] = item;
+        this.nextLast = index_increment(nextLast,+1);
+        size ++;
+    }
+    public boolean isEmpty(){
+        return size == 0;
+    }
+    public int size(){
+        return size;
+    }
+    public void printDeque(){
+        int index = index_increment(nextFirst,+1);
+        while (index != nextLast){
+            System.out.print(array[index] + " ");
+            index = index_increment(index,+1);
+        }
+        System.out.println();
+    }
+    public T removeFirst(){
+        if(size != 0) {
+            this.nextFirst = index_increment(nextFirst,+1);
+        }
+        size -- ;
+        if(array.length >= 16) {
+            this.usage_check();
+        }
+        return array[nextFirst];
+    }
+    public T removeLast(){
+        if(size != 0){
+            this.nextLast = index_increment(nextLast,-1);
+        }
+        size -- ;
+        if(array.length >= 16) {
+            this.usage_check();
+        }
+        return array[nextLast];
+    }
+    public T get(int index){
+        T item;
+        int index_now = nextFirst;
+        int i = 0 ;
+        while( i <= index){
+            index_now = index_increment(index_now,+1);
+            i++;
+        }
+        item = array[index_now];
+        return item;
+    }
+    private  void size_revise(int new_length){
+        //Hardest and most important method in this class.
+        //1.expand
+        if(new_length > array.length){
+            T[] new_array = (T[]) new Object[new_length];
+            int incre = new_length - array.length;
+            System.arraycopy(array,0,new_array,0,nextLast);
+            System.arraycopy(array,nextFirst+1,new_array,nextLast+incre,array.length-nextFirst-1);
+            nextFirst = nextLast+incre-1;
+            array = new_array;
+        }
+        //2.contract
+        if(new_length< array.length){
+            T[] new_array = (T[]) new Object[new_length];
+            int index_now = index_increment(nextFirst,+1);
+            int i = 0;
+            while(index_now != nextLast){
+                new_array[i] = array[index_now];
+                index_now = index_increment(index_now,+1);
+            }
+            array = new_array;
+            nextFirst = new_length-1;
+            nextLast = size - 1;
+        }
+    }
+    private void usage_check(){
+        if(((double)size/array.length) < usage_limit){
+            if(usage_limit < 0.5) {
+                this.size_revise(size * 2);
+            }else{
+                this.size_revise(Math.max(size,(int) ((double)size/usage_limit)));
+            }
+        }
+    }
+    private boolean capacity_check(){
+        return size == array.length;
+    }
 }
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,2 +1,90 @@
-package PACKAGE_NAME;public class LinkedListDeque {
+public class LinkedListDeque<Item>{
+    private class node{
+        node p_next;
+        node p_last;
+        Item data;
+        public node(node next,node last,Item data){
+            p_next = next;
+            p_last = last;
+            this.data = data;
+        }
+    }
+    private int size;
+    private node sentinel;
+    public LinkedListDeque(Item data){
+        this.size = 1;
+        this.sentinel = new node(null,null,data);
+        //This "data" above is meaningless and will not be used.
+        sentinel.p_next = new node(sentinel,sentinel,data);
+        sentinel.p_last = sentinel.p_next;
+    }
+    public LinkedListDeque(){
+        this.size = 0;
+        this.sentinel = new node(null,null,null);
+    }
+    public void addFirst(Item data){
+        node new_node = new node(sentinel.p_next,sentinel,data);
+        sentinel.p_next = new_node;
+        new_node.p_next.p_last = new_node;
+        size++;
+    }
+    public void addLast(Item data){
+        node new_node = new node(sentinel,sentinel.p_last,data);
+        new_node.p_last.p_next = new_node;
+        sentinel.p_last = new_node;
+        size++;
+    }
+
+    public int size(){
+        return this.size;
+    }
+    public boolean isEmpty(){
+        if(sentinel.p_next == sentinel){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public void printDeque(){
+        node ptr = sentinel.p_next;
+        while(ptr != sentinel){
+            System.out.print(ptr.data+" ");
+            ptr = ptr.p_next;
+        }
+        System.out.println();
+    }
+    public Item removeFirst(){
+        if(size == 0){
+            return null;
+        }
+        node save = sentinel.p_next;
+        save.p_next.p_last = sentinel;
+        sentinel.p_next = save.p_next;
+        size --;
+        return save.data;
+    }
+    public Item removeLast(){
+        if(size == 0){
+            return null;
+        }
+        node save = sentinel.p_last;
+        save.p_last.p_next =sentinel;
+        sentinel.p_last = save.p_last;
+        size --;
+        return save.data;
+    }
+    public Item get(int index){
+       if(index >= size){
+           return null;
+       }
+       int i = 0;
+       Item data = null;
+       node ptr = sentinel.p_next;
+       while(i <= index){
+           data = ptr.data;
+           ptr = ptr.p_next;
+           i++;
+       }
+       return data;
+    }
 }
